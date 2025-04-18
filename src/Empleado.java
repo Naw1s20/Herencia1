31366fa1d442443825b8dea48083336a8d9b2205
Alex <alejandronrnewey@hotmail.es>
2025-04-18 12:51:37 +0200

    primer commit

---
+++ src/Empleado.java
@@ -0,0 +1,30 @@
+public class Empleado extends Persona
+
+{
+    private double remuneracion;
+    private int empleadoId;
+
+
+    public void aumentarRemuneracion(int aumento){
+
+    }
+
+    public Empleado() {
+    }
+
+    public double getRemuneracion() {
+        return remuneracion;
+    }
+
+    public void setRemuneracion(double remuneracion) {
+        this.remuneracion = remuneracion;
+    }
+
+    public int getEmpleadoId() {
+        return empleadoId;
+    }
+
+    public void setEmpleadoId(int empleadoId) {
+        this.empleadoId = empleadoId;
+    }
+}
